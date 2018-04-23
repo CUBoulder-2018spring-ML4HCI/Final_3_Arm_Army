af87ffb1080edcb1fef307202290e1f5cba0f69f
aurbanski <aurbanski27@gmail.com>
2018-04-22 20:22:47 -0600

    Wrote some more bad styles

--- Electron/ArmGUI/app/components/Action.js
+++ Electron/ArmGUI/app/components/Action.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Action.css'
 
 type Props = {};
 
@@ -12,9 +13,14 @@ export default class Action extends Component<Props> {
         <Link to="/motion">
           <i className="fa fa-arrow-left fa-3x" />
         </Link>
-        <h1>Record a motion</h1>
+        <h1 className={styles.header}>Record a motion</h1>
         <h2>Hold down the button to record</h2>
-        <Link to="/">Save Mapping</Link>
+        <div className={styles.center}>
+          <button className={styles.btn}>Record</button>
+        </div>
+        <div className={styles.center}>
+          <Link className={styles.sub_btn} to="/">Save Mapping</Link>
+        </div>
       </div>
     );
   }
