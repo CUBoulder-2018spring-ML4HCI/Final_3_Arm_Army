d007ee1372dc75cac08686a9ec49f2a8758035ea
Rachel Platt <rachel.s.platt@colorado.edu>
2018-04-23 13:49:02 -0600

    Added comments to serial program

--- MicroBitRadioStuff/notserial.py
+++ MicroBitRadioStuff/notserial.py
@@ -2,7 +2,7 @@ import serial
 import time
 import OSC
 
-send_address = '10.201.30.13', 6448
+send_address = '127.0.0.1', 6448
 
 PORT="/dev/cu.usbmodem1412"
 BAUD = 115200
@@ -18,17 +18,20 @@ while True:
     #print(intx)
     if (len(intx)==4):
         try:
+            #check if values can be converted to float
             x=float(intx[0])
             y=float(intx[1])
             z=float(intx[2])
             print (x, y, z)
             rNum = OSC.OSCMessage()
-            rNum.setAddress("/wek/inputs")# get a random num every loop
-            rNum.append(x) #0.0 here is hack to make it float
+            rNum.setAddress("/wek/inputs")
+            #append values to osc message
+            rNum.append(x)
             rNum.append(y)
             rNum.append(z)
             c.send(rNum)
         except ValueError:
+            #if they cant be flaots than skip over to the next value 
             continue
 
     #print(x)
